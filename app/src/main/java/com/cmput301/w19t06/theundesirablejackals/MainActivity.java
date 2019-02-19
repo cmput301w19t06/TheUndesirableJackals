f8f42092749141d504208c19f9c5a962b929f133
Makata <limbaga@ualberta.ca>
2019-02-19 00:37:17 -0700

    created barcode scanner

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/MainActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/MainActivity.java
@@ -1,13 +1,116 @@
 package com.cmput301.w19t06.theundesirablejackals;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SurfaceView svBarcode;
+    private TextView tvBarcode;
+
+    private BarcodeDetector detector;
+    private CameraSource cameraSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        svBarcode = findViewById(R.id.sv_barcode);
+        tvBarcode = findViewById(R.id.tv_barcode);
+
+        detector = new BarcodeDetector.Builder(this).setBarcodeFormats(Barcode.ALL_FORMATS).build();
+        detector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                 final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+                 if(barcodes.size()>0) {
+                     tvBarcode.post(new Runnable() {
+                         @Override
+                         public void run() {
+                             tvBarcode.setText(barcodes.valueAt(0).displayValue);
+                         }
+                     });
+                 }
+            }
+        });
+
+        cameraSource = new CameraSource.Builder(this, detector).setRequestedPreviewSize(1024,768)
+                .setRequestedFps(25).setAutoFocusEnabled(true).build();
+        svBarcode.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED) {
+                    try {
+                        cameraSource.start(holder);
+                    } catch (IOException e) {
+                        // TODO: IOException
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.CAMERA}, 123);
+                }
+            }
+            @Override
+
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode==123) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                try {
+                    cameraSource.start(svBarcode.getHolder());
+                } catch (IOException e) {
+                    // TODO: IOException
+                }
+            } else {
+                Toast.makeText(this, "Scanner wont work without permission!",
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        detector.release();
+        cameraSource.stop();
+        cameraSource.release();
     }
 }
