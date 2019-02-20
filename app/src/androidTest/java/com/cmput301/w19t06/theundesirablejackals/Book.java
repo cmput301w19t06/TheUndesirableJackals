5542f7902fddada88ad6c862245ff8f54c73f115
Felipe <35282386+2663041@users.noreply.github.com>
2019-02-20 11:16:35 -0700

    Add files via upload

---
+++ app/src/androidTest/java/com/cmput301/w19t06/theundesirablejackals/Book.java
@@ -0,0 +1,69 @@
+
+
+public class Book {
+    private String title;
+    private String author;
+    private String isbn;
+    private String status; // available, requested, accepted or borrowed
+    private User owner;
+    private Image image;
+
+    public Book(String title, String author, String isbn, User owner) {
+        // status is set to "available" as default
+        // images is set to null as default
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.status = "available";
+        this.owner = owner;
+        this.image = null;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getISBN() {
+        return isbn;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setTitle(String newTitle) {
+        title = newTitle;
+    }
+
+    public void setAuthor(String newAuthor) {
+        author = newAuthor;
+    }
+
+    public void setISBN(String newISBN) {
+        isbn = newISBN;
+    }
+
+    public void setStatus(String newStatus) {
+        status = newStatus;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public void deleteImage() {
+        image = null;
+    }
+}
---
+++ app/src/androidTest/java/com/cmput301/w19t06/theundesirablejackals/Image.java
@@ -0,0 +1,10 @@
+
+
+public class Image {
+    private String fileLocation;
+
+    public Image(String fileLocation) {
+        // add check valid format here in the constructor
+        this.fileLocation = fileLocation;
+    }
+}
