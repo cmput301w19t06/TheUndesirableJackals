d3dc14ddd911c1f36c44111845abc3a15abcf933
deweertd <deweertd1830@gmail.com>
2019-02-21 18:55:07 -0700

    Fix string compare

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Authentication.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Authentication.java
@@ -15,7 +15,10 @@ public class Authentication {
 
     public Boolean authenticate(String input) {
         // Returns true if user's input is the same as password
-        return password == input;
+        //NOT IT DOES NOT  IT RETURNS TRUE IF THE PASSWORD AND THE INPUT ARE THE LITERAL SAME STRING!!!!!!
+        //return password == input;
+
+        return password.equals(input); //Strings are not primitive "int's", don't use ==
     }
 
     public String getUserName() {
