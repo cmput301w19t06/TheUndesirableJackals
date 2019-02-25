cf80b63f8a0545ba018aee19a53deec46cb24684
Deweertd <deweertd1830@gmail.com>
2019-02-25 12:06:03 -0700

    Added working camera with bar code scanner

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/MainActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/MainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -14,6 +16,7 @@ import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,6 +24,8 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
@@ -28,8 +33,15 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -42,9 +54,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FirebaseDatabase database;
     private DatabaseReference myRef;
     private StorageReference mStorageRef;
+    private Bitmap imageBitmap;
     private static final int RC_SIGN_IN = 123;
+    private static final int REQUEST_IMAGE_CAPTURE = 321;
     private static final String TAG = "CustomAuthActivity";
     private TokenBroadcastReceiver mTokenReceiver;
+    FirebaseVisionBarcodeDetectorOptions options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,8 +72,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Button click listeners
         findViewById(R.id.buttonSignIn).setOnClickListener(this);
+        findViewById(R.id.buttonScanBarcode).setOnClickListener(this);
 
 
+
+    }
+
+    protected void createBarcodeScanner(){
+        options =
+                new FirebaseVisionBarcodeDetectorOptions.Builder()
+                        .setBarcodeFormats(
+                                FirebaseVisionBarcode.FORMAT_EAN_13)
+                        .build();
     }
 
 
@@ -103,6 +128,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 // response.getError().getErrorCode() and handle the error.
                 // ...
             }
+        }else if(requestCode == REQUEST_IMAGE_CAPTURE){
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                imageBitmap = (Bitmap) extras.get("data");
+                scanBArcodePII();
+
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
         }
     }
 
@@ -112,11 +150,59 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if (i == R.id.buttonSignIn) {
             signIn();
 
+        }else if(i == R.id.buttonScanBarcode){
+            scanBarcode();
+        }
+    }
+
+    private void dispatchTakePictureIntent() {
+
+
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
     private void signIn(){
         createSignInIntent();
+    }
+
+    private void scanBarcode(){
+        createBarcodeScanner();
+        dispatchTakePictureIntent();
+
+    }
+
+    private void scanBArcodePII(){
+        ((ImageView)findViewById(R.id.imageViewBarcodePicture)).setImageBitmap(imageBitmap);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(imageBitmap);
+        FirebaseVisionBarcodeDetector detector = FirebaseVision.getInstance()
+                .getVisionBarcodeDetector(options);
+
+
+        Task<List<FirebaseVisionBarcode>> result = detector.detectInImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                    @Override
+                    public void onSuccess(List<FirebaseVisionBarcode> barcodes) {
+                        // Task completed successfully
+                        // ...
+                        if(barcodes.size() > 0){
+                            ((TextView)findViewById(R.id.textViewBarcode)).setText(barcodes.get(0).toString());
+                        }else{
+                            ((TextView)findViewById(R.id.textViewBarcode)).setText("Good pic, no code tho");
+                        }
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        // ...
+                        ((TextView)findViewById(R.id.textViewBarcode)).setText("FAILURE");
+                    }
+                });
     }
 
 
