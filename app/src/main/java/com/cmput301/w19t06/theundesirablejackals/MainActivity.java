316703737308594a4c73e002c17cdb24eda7fe5c
deweertd <deweertd1830@gmail.com>
2019-03-03 15:14:21 -0700

    US Cleanup in barcode getter

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/MainActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/MainActivity.java
@@ -12,6 +12,7 @@ import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 
