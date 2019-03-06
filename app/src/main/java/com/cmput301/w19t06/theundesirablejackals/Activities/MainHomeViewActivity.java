3463240fb46c6be3d4e1382489fd7d8032893166
Mohamed Ahmed <maxamednajiib10@hotmail.com>
2019-03-05 21:20:39 -0700

    added menu icon, created a new layout for toolbar

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Activities/MainHomeViewActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Activities/MainHomeViewActivity.java
@@ -4,14 +4,17 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 import com.cmput301.w19t06.theundesirablejackals.Activities.AddBookActivity;
 import com.cmput301.w19t06.theundesirablejackals.Adapter.ViewPagerAdapter;
@@ -33,9 +36,15 @@ public class MainHomeViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_home_view);
         tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
-        //toolBar = findViewById(R.id.tool_bar);
+        toolBar = (Toolbar) findViewById(R.id.tool_bar);
         navigationView = findViewById(R.id.navigationView);
         drawerLayout = findViewById(R.id.drawer_layout);
+        setSupportActionBar(toolBar);
+        getSupportActionBar().setTitle(null);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //enable home button on the app i.e it adds home button to the app
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu);
 
         databaseHelper = new DatabaseHelper(MainHomeViewActivity.this);
 
@@ -121,5 +130,18 @@ public class MainHomeViewActivity extends AppCompatActivity {
     public void addBookButton(View view){
         Intent intent = new Intent(this, AddBookActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // open navigation drawer by tabbing the menu icon
+        switch (item.getItemId()){
+
+            case android.R.id.home:
+                  drawerLayout.openDrawer(GravityCompat.START);
+                  return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Book/BookRequestStatus.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Book/BookRequestStatus.java
@@ -4,7 +4,7 @@ public enum BookRequestStatus {
     PENDING,            // book has been requested and waiting for owner to take action
     DENIED,             // book requested by the borrower is denied by the owner
     ACCEPTED,           // book request is accepted by book owner
-    HANDED_OFF,         // book has been scanned for hand off by the owner
+    HANDED_OFF,        // book has been scanned for hand off by the owner
     RECEIVED_BORROWER,  // book has been scanned and received by the borrower
     RETURNING,          // book has been scanned for return by the borrower
     RECEIVED_OWNER      // returned book has been received by the owner
