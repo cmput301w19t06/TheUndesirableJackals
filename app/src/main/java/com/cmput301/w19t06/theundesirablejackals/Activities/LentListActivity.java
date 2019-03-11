99d29c3dfb0323acc7e48b703a4de4af1e457804
Kaya <kthiesse@ualberta.ca>
2019-03-11 05:44:29 -0600

    Lent List improvements. Still need implementation on building the list. Added intermediate page of accepting/rejecting offers.

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Activities/LentListActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/Activities/LentListActivity.java
@@ -2,6 +2,9 @@ package com.cmput301.w19t06.theundesirablejackals.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 /**
  * List view of all current lent requests. Allow the user to view more about them
@@ -12,5 +15,12 @@ public class LentListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lend_requets);
+        String[] lendOptions = {};
+        //TODO
+        //Pull values that are lend requests and add to lendOptions
+
+        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, lendOptions);
+        ListView listview = (ListView) findViewById(R.id.lendRequest_list);
+        listview.setAdapter(adapter);
     }
 }
