bfe496940b96b429eb4fdcc87024e3165149d4b8
Deweertd <deweertd1830@gmail.com>
2019-03-15 12:35:11 -0600

    Friends list recycler view adapter

---
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/adapter/FriendsRecyclerViewAdapter.java
@@ -0,0 +1,6 @@
+package com.cmput301.w19t06.theundesirablejackals.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+public class FriendsRecyclerViewAdapter extends RecyclerView.Adapter {
+}
