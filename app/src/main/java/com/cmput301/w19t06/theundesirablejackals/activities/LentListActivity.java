27b19033e94ab4dbd3b3770a572246a54c0edcf0
kthiesse <kthiesse@ualberta.ca>
2019-03-19 17:25:23 -0600

    Added back button in notification, Borrowed, Lent

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/LentListActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/LentListActivity.java
@@ -3,6 +3,7 @@ package com.cmput301.w19t06.theundesirablejackals.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,14 +15,26 @@ import android.widget.ListView;
  * Author: Kaya Thiessen
  */
 public class LentListActivity extends AppCompatActivity{
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lend_requets);
-        String[] lendOptions = {};
-        //TODO
-        //Pull values that are lend requests and add to lendOptions
 
+        toolbar = findViewById(R.id.tool_barLend);
+        toolbar.setNavigationIcon(R.drawable.ic_action_back);
+        toolbar.setTitle("Lent Requests");
+        setSupportActionBar(toolbar);
+
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainHomeViewActivity.class));
+                finish();
+            }
+        });
     }
 
 }
--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/NotificationActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/NotificationActivity.java
@@ -3,6 +3,7 @@ package com.cmput301.w19t06.theundesirablejackals.activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,7 +16,7 @@ import android.widget.ListView;
  * Author: Kaya Thiessen
  */
 public class NotificationActivity extends AppCompatActivity {
-    private String[] notifications= {};
+    private Toolbar toolbar;
 
     /**
      * On create method
@@ -25,6 +26,19 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_view);
+
+        toolbar = findViewById(R.id.tool_barNotification);
+        toolbar.setNavigationIcon(R.drawable.ic_action_back);
+        toolbar.setTitle("Notifications");
+        setSupportActionBar(toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), MainHomeViewActivity.class));
+                finish();
+            }
+        });
     }
 
 }
