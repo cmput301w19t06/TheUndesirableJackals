51962427f7e6b6dc9b8a96d906fcdbff966c9f25
Kaya <kthiesse@ualberta.ca>
2019-03-20 15:41:11 -0600

    Added menu to lend

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/LentListActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/LentListActivity.java
@@ -4,11 +4,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import com.cmput301.w19t06.theundesirablejackals.classes.ToastMessage;
 
 /**
  * List view of all current lent requests. Allow the user to view more about them
@@ -35,6 +40,39 @@ public class LentListActivity extends AppCompatActivity{
                 finish();
             }
         });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_lend_request, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.itemMenuLentLent:
+                ToastMessage.show(this, "Viewing Currently Lent...");
+                break;
+            case R.id.itemMenuLentAccepted:
+                ToastMessage.show(this, "Viewing Accepted...");
+                break;
+            case R.id.itemMenuLentRequested:
+                ToastMessage.show(this, "Viewing Requested...");
+                break;
+            case R.id.itemMenuOwnedBookDelete:
+                ToastMessage.show(this, "Title Search");
+                break;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
