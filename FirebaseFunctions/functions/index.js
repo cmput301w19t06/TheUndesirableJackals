e9bf5736ea9a5485987eb3efaadc557ed1fddbcf
deweertd <deweertd1830@gmail.com>
2019-03-20 20:18:45 -0600

    firebase notification testing

--- FirebaseFunctions/functions/index.js
+++ FirebaseFunctions/functions/index.js
@@ -11,49 +11,62 @@ exports.sendNotifications = functions.database.ref('/messages/{user_name}/{notif
   const from_user = context.params.from_user;
 
   console.log(user_name, ' has a new message');
-  if(user_name != from_user){}
+  if(user_name !== from_user){
     const payload = {
       notification: {
         title: "New Message!",
-        body: "Received message from " + from_user,
+        body: "Received message",
         icon: "default"
       }
     };
-    return admin.messaging().sendToDevice(/*Token ID */, payload).then(response);
   }
   return true;
 });
 
-exports.sendLendRequestNotification = functions.database.ref('/requests/lendRequest/{user_name}/{notification_id}/borrower/userName/{borrower}').onWrite((change, context)=>{
+exports.sendLendRequestNotification = functions.database.ref('/requests/lendRequest/{user_name}/{notification_id}').onWrite((change, context)=>{
   const user_name = context.params.user_name;
   const notification = context.params.notification_id;
   const borrower = context.params.borrower;
 
   console.log(user_name, ' has a new lend Request');
-  const payload = {
-    notification: {
-      title: "New Lend Request!",
-      body: "Received from " + from_user,
-      icon: "default"
-    }
-  };
 
-  return admin.messaging().sendToDevice(/*Token ID */, payload).then(response);
+  const deviceToken = admin.database().ref(`/deviceToken/${user_name}`).once('value');
+  return deviceToken.then((result)=> {
+      const token_id = result.val();
+      const payload = {
+        notification: {
+          title: "New Lend Request!",
+          body: "Received update",
+          icon: "default"
+        }
+      };
+      return admin.messaging().sendToDevice(token_id, payload).then((response)=>{
+        return console.log(token_id);
+      });
+
+  })
 });
 
-exports.sendBorrowRequestNotification = functions.database.ref('/requests/borrowRequest/{user_name}/{notification_id}/bookRequested/owner/{owner}').onWrite((change, context)=>{
+exports.sendBorrowRequestNotification = functions.database.ref('/requests/borrowRequest/{user_name}/{notification_id}').onWrite((change, context)=>{
   const user_name = context.params.user_name;
   const notification = context.params.notification_id;
   const owner = context.params.owner;
 
   console.log(user_name, ' has a new borrow Request');
-  const payload = {
-    notification: {
-      title: "New Borrow Request!",
-      body: "Received from " + owner,
-      icon: "default"
-    }
-  };
 
-  return admin.messaging().sendToDevice(/*Token ID */, payload).then(response);
+  const deviceToken = admin.database().ref(`/deviceToken/${user_name}`).once('value');
+  return deviceToken.then((result)=> {
+    const payload = {
+      notification: {
+        title: "New Borrow Request Status!",
+        body: "Received update",
+        icon: "default"
+      }
+    };
+    const token_id = result.val();
+    return admin.messaging().sendToDevice(token_id, payload).then((response)=>{
+      return console.log(token_id);
+    });
+
+  })
 });
