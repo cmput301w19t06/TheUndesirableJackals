db998ae580966a74b364f72e09b1fc45d5e4fbe5
Makata <limbaga@ualberta.ca>
2019-03-21 11:25:46 -0600

    Library Book view works but still needs polishing

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/fragment/LibraryFragment.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/fragment/LibraryFragment.java
@@ -28,6 +28,7 @@ import com.cmput301.w19t06.theundesirablejackals.adapter.SwipeController;
 import com.cmput301.w19t06.theundesirablejackals.book.Book;
 import com.cmput301.w19t06.theundesirablejackals.book.BookInformationList;
 import com.cmput301.w19t06.theundesirablejackals.book.BookList;
+import com.cmput301.w19t06.theundesirablejackals.book.BookRequest;
 import com.cmput301.w19t06.theundesirablejackals.classes.ToastMessage;
 import com.cmput301.w19t06.theundesirablejackals.database.BookInformationListCallback;
 import com.cmput301.w19t06.theundesirablejackals.database.BookListCallback;
@@ -116,46 +117,6 @@ public class LibraryFragment extends Fragment implements SwipeRefreshLayout.OnRe
                 });
             }
         };
-
-//        RecyclerViewClickListener listener = new RecyclerViewClickListener() {
-//            @Override
-//            public void onClick(View view, final int position) {
-//                Book clickedBook = libraryRecyclerViewAdapter.getBook(position);
-//                final DatabaseHelper databaseHelper = new DatabaseHelper();
-//                databaseHelper.getAllBookInformations(clickedBook, new BookInformationListCallback() {
-//                    @Override
-//                    public void onCallback(BookInformationList bookInformationList) {
-//                        if(bookInformationList != null){
-//                            final BookInformation bookInformation = bookInformationList.get(0);
-//                            databaseHelper.getCurrentUserInfoFromDatabase(new UserInformationCallback() {
-//                                @Override
-//                                public void onCallback(UserInformation userInformation) {
-//                                    BookRequest bookRequest = new BookRequest(userInformation, bookInformation);
-//                                    databaseHelper.makeBorrowRequest(bookRequest, new BooleanCallback() {
-//                                        @Override
-//                                        public void onCallback(boolean bool) {
-//                                            Toast.makeText(getActivity(), "Library book clicked at " + ((Integer) position).toString(), Toast.LENGTH_LONG).show();
-//                                            if(bool){
-//                                                Toast.makeText(getActivity(), "Request sent to " + bookInformation.getOwner(), Toast.LENGTH_LONG).show();
-//                                                Intent intent = new Intent(getActivity(), ShowBookOwnersActivity.class);
-////                                                intent.putExtra(ViewOwnedBookActivity.OWNED_BOOK_FROM_RECYCLER_VIEW, clickedBook);
-////                                                intent.putExtra(ViewOwnedBookActivity.OWNED_INFO_FROM_RECYCLER_VIEW, clickedbookInformation);
-//                                                startActivity(intent);
-//                                            }else{
-//                                                Toast.makeText(getActivity(), "Request not sent", Toast.LENGTH_LONG).show();
-//                                            }
-//                                        }
-//                                    });
-//                                }
-//                            });
-//
-//                        }
-//                    }
-//                });
-//                //Do something with the book, maybe view it in detail?
-//                Toast.makeText(getActivity(), "Library book clicked at " + ((Integer) position).toString(), Toast.LENGTH_LONG).show();
-//            }
-//        };
 
         //create the adapter to manage the data and the recyclerView,
         //give it the above listener
