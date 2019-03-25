f266f9cfcb01d35eb51705e9efdcc2387971153e
Art Limbaga <limbaga@ualberta.ca>
2019-03-25 17:08:28 -0600

    Doing show book owners

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/ShowBookOwnersActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/ShowBookOwnersActivity.java
@@ -4,18 +4,25 @@ import android.content.Intent;
 import android.database.DataSetObservable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.util.DisplayMetrics;
 
+import com.cmput301.w19t06.theundesirablejackals.adapter.RequestsRecyclerViewAdapter;
 import com.cmput301.w19t06.theundesirablejackals.book.Book;
 import com.cmput301.w19t06.theundesirablejackals.book.BookInformation;
 import com.cmput301.w19t06.theundesirablejackals.book.BookInformationList;
+import com.cmput301.w19t06.theundesirablejackals.database.DatabaseHelper;
 
 public class ShowBookOwnersActivity extends AppCompatActivity {
     public static final String BOOK_OBJECT = "BookObject";
     public static final String LIST_OF_OWNERS = "ListOfOwners";
 
+    // ratio in relation to the original display
     private final Double WIDTH_RATIO = 0.8;
     private final Double HEIGHT_RATIO = 0.85;
+
+    private DatabaseHelper mDatabaseHelper;
+    private RequestsRecyclerViewAdapter mRequestsRecyclerViewAdapter;
 
     private Book mClickedBook;
     private BookInformationList mClickedBookOwners;
@@ -39,6 +46,8 @@ public class ShowBookOwnersActivity extends AppCompatActivity {
         Intent intent = getIntent();
         mClickedBook = (Book) intent.getSerializableExtra(BOOK_OBJECT);
         mClickedBookOwners = (BookInformationList) intent.getSerializableExtra(LIST_OF_OWNERS);
+
+        RecyclerView recyclerView;
 
 
     }
