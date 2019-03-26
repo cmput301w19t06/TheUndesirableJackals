002cb88a681dc311dcee2105b3e51abf777d060e
Deweertd <deweertd1830@gmail.com>
2019-03-26 16:13:36 -0600

    Quick interface rename

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/database/UriCallback.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/database/UriCallback.java
@@ -2,6 +2,6 @@ package com.cmput301.w19t06.theundesirablejackals.database;
 
 import android.net.Uri;
 
-public interface uriCallback {
+public interface UriCallback {
     void onCallback(Uri uri);
 }
