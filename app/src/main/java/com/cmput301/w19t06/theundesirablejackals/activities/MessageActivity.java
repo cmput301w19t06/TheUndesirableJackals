c188d5dd47cf90f85ede34c6483f13b5ca3d4e56
Deweertd <deweertd1830@gmail.com>
2019-03-26 16:38:07 -0600

    working on showing all conversations

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/MessageActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/MessageActivity.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 
 import com.cmput301.w19t06.theundesirablejackals.adapter.MessagesRecyclerViewAdapter;
 import com.cmput301.w19t06.theundesirablejackals.adapter.RecyclerViewClickListener;
+import com.cmput301.w19t06.theundesirablejackals.classes.MessageMetaData;
 import com.cmput301.w19t06.theundesirablejackals.classes.Messaging;
 import com.cmput301.w19t06.theundesirablejackals.classes.ToastMessage;
 import com.cmput301.w19t06.theundesirablejackals.database.BooleanCallback;
@@ -89,6 +90,56 @@ public class MessageActivity extends AppCompatActivity implements RecyclerViewCl
     @Override
     public void onClick(View view, int position) {
         showToast("Clicked on " + position);
+        MessageMetaData metaData = messagesRecyclerViewAdapter.getDataSet().get(position);
+        showMessagesAlertBox("Messages with " + metaData.getUsername());
+    }
+
+    private void showMessagesAlertBox(String title) {
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.messages_popup_alert);
+        dialog.setCanceledOnTouchOutside(true);
+        TextView titleView = (TextView) dialog.findViewById(R.id.textViewNewMessageAlertTitle);
+        titleView.setText(title);
+
+        final Button send = (Button) dialog.findViewById(R.id.buttonNewMessageAlertSend);
+        Button cancel = (Button) dialog.findViewById(R.id.buttonNewMessageAlertCancel);
+
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code the functionality when send button is clicked
+                EditText recipientView = (EditText) dialog.findViewById(R.id.editTextNewMessageAlertUser);
+                EditText messageView = (EditText) dialog.findViewById(R.id.editTextNewMessageAlertMessage);
+                final String message = messageView.getText().toString();
+                String recipient = recipientView.getText().toString();
+                if(!message.isEmpty() && !recipient.isEmpty()) {
+                    databaseHelper.getUserInfoFromDatabase(recipient, new UserInformationCallback() {
+                        @Override
+                        public void onCallback(UserInformation userInformation) {
+                            if (userInformation != null) {
+                                sendMessage(message, userInformation.getUserName());
+                            } else {
+                                showToast("User not found");
+                            }
+                        }
+                    });
+                }else{
+                    showToast("Please fill missing fields");
+                }
+                dialog.dismiss();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code the functionality when NO button is clicked
+                dialog.dismiss();
+            }
+        });
+
+        dialog.show();
     }
 
 
@@ -97,18 +148,18 @@ public class MessageActivity extends AppCompatActivity implements RecyclerViewCl
     public void onClick(View v) {
         if(currentUser != null) {
 //            showToast("Clicked on Floating Action button");
-            showNewMessageAlertbox("New Personal Message");
+            showNewMessageAlertBox("New Personal Message");
 
         }else{
             showToast("Something happened when talking to the database");
         }
     }
 
-    public void showToast(String message){
+    private void showToast(String message){
         ToastMessage.show(this, message);
     }
 
-    public void showNewMessageAlertbox(String title) {
+    private void showNewMessageAlertBox(String title) {
         final Dialog dialog = new Dialog(this);
         dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         dialog.setContentView(R.layout.new_message_popup_alert);
