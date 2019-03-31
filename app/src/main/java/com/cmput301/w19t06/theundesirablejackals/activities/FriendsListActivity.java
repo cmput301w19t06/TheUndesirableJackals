309257e6f9a3b7a42a5be9b2710d14a4545edd97
kthiesse <kthiesse@ualberta.ca>
2019-03-30 18:03:30 -0600

    Friends request Added. Still needs to be populated with proper data and button controls

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/FriendsListActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/FriendsListActivity.java
@@ -2,22 +2,24 @@ package com.cmput301.w19t06.theundesirablejackals.activities;
 
 import android.content.BroadcastReceiver;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.View;
 
-import com.cmput301.w19t06.theundesirablejackals.adapter.FriendRequestRecyclerViewAdapter;
+import com.cmput301.w19t06.theundesirablejackals.adapter.FriendsRecyclerViewAdapter;
 import com.cmput301.w19t06.theundesirablejackals.adapter.RecyclerViewClickListener;
 import com.cmput301.w19t06.theundesirablejackals.adapter.SwipeController;
+import com.cmput301.w19t06.theundesirablejackals.book.BookRequestList;
 import com.cmput301.w19t06.theundesirablejackals.classes.CurrentActivityReceiver;
-import com.cmput301.w19t06.theundesirablejackals.classes.ToastMessage;
 import com.cmput301.w19t06.theundesirablejackals.classes.FriendRequest;
+import com.cmput301.w19t06.theundesirablejackals.classes.ToastMessage;
+import com.cmput301.w19t06.theundesirablejackals.database.BookRequestListCallback;
 import com.cmput301.w19t06.theundesirablejackals.database.DatabaseHelper;
 import com.cmput301.w19t06.theundesirablejackals.database.FriendRequestListCallback;
 import com.cmput301.w19t06.theundesirablejackals.database.UserCallback;
@@ -29,34 +31,38 @@ import com.cmput301.w19t06.theundesirablejackals.user.UserList;
 
 import java.util.ArrayList;
 
-public class ViewFriendRequestsActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, RecyclerViewClickListener {
-    private FriendRequestRecyclerViewAdapter friendRequestRecyclerViewAdapter = new FriendRequestRecyclerViewAdapter();
+/**
+ * List of friends to the user. Allows them to add new users and view suggested users.
+ * From here they can access friends profiles
+ * Author: Kaya Thiessen
+ */
+public class FriendsListActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, RecyclerViewClickListener {
+    private FriendsRecyclerViewAdapter friendsRecyclerViewAdapter = new FriendsRecyclerViewAdapter();
     private BroadcastReceiver currentActivityReceiver;
     private SwipeRefreshLayout swipeRefreshLayout;
     private DatabaseHelper databaseHelper;
     private UserInformation currentUser;
     private Toolbar toolbar;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_friend_requests);
+        setContentView(R.layout.activity_friends_list);
         databaseHelper = new DatabaseHelper();
 
         currentActivityReceiver = new CurrentActivityReceiver(this);
         LocalBroadcastManager.getInstance(this).
                 registerReceiver(currentActivityReceiver, CurrentActivityReceiver.CURRENT_ACTIVITY_RECEIVER_FILTER);
 
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewFriendRequestListActivityFriends);
-        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activitySwipeFriendRequestListRefreshLayout);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewFriendsListActivityFriends);
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.activitySwipeFriendsListRefreshLayout);
 
         ItemTouchHelper itemTouchHelper;
         SwipeController swipeController;
 
-        toolbar = findViewById(R.id.tool_barFriendRequestList);
+        toolbar = findViewById(R.id.tool_barFriendsList);
         toolbar.setNavigationIcon(R.drawable.ic_action_back);
-        toolbar.setTitle("Friend Requests");
+        toolbar.setTitle("Friends");
         setSupportActionBar(toolbar);
 
         recyclerView.setHasFixedSize(true);
@@ -64,8 +70,8 @@ public class ViewFriendRequestsActivity extends AppCompatActivity implements Swi
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        friendRequestRecyclerViewAdapter.setMyListener((RecyclerViewClickListener) this);
-        recyclerView.setAdapter(friendRequestRecyclerViewAdapter);
+        friendsRecyclerViewAdapter.setMyListener((RecyclerViewClickListener) this);
+        recyclerView.setAdapter(friendsRecyclerViewAdapter);
         swipeRefreshLayout.setOnRefreshListener((SwipeRefreshLayout.OnRefreshListener) this);
         swipeRefreshLayout.post(new Runnable() {
             @Override
@@ -74,7 +80,7 @@ public class ViewFriendRequestsActivity extends AppCompatActivity implements Swi
             }
         });
 
-        swipeController = new SwipeController(friendRequestRecyclerViewAdapter);
+        swipeController = new SwipeController(friendsRecyclerViewAdapter);
         itemTouchHelper = new ItemTouchHelper(swipeController);
         itemTouchHelper.attachToRecyclerView(recyclerView);
 
@@ -102,27 +108,8 @@ public class ViewFriendRequestsActivity extends AppCompatActivity implements Swi
     @Override
     public void onRefresh() {
         swipeRefreshLayout.setRefreshing(true);
-        getFriendRequestList();
+        getFriendsList();
         swipeRefreshLayout.setRefreshing(false);
-    }
-
-    private void getFriendRequestList() {
-        friendRequestRecyclerViewAdapter.setDataSet(new ArrayList<FriendRequest>());
-        databaseHelper.getCurrentUserInfoFromDatabase(new UserInformationCallback() {
-            @Override
-            public void onCallback(UserInformation user) {
-                if(user != null){
-                    databaseHelper.getReceivedFriendRequests(user, new FriendRequestListCallback() {
-                        @Override
-                        public void onCallback(ArrayList<FriendRequest> friendRequests) {
-                            if(friendRequests != null){
-                                friendRequestRecyclerViewAdapter.setDataSet(friendRequests);
-                            }
-                        }
-                    });
-                }
-            }
-        });
     }
 
     @Override
@@ -151,9 +138,27 @@ public class ViewFriendRequestsActivity extends AppCompatActivity implements Swi
     }
 
 
+    private void getFriendsList() {
+        friendsRecyclerViewAdapter.setDataSet(new UserList());
+        databaseHelper.getCurrentUserFromDatabase(new UserCallback() {
+            @Override
+            public void onCallback(User user) {
+                if(user != null){
+                    databaseHelper.getFriendsList(user.getUserInfo().getUserName(), new UserListCallback() {
+                        @Override
+                        public void onCallback(UserList userList) {
+                            if(userList != null){
+                                friendsRecyclerViewAdapter.setDataSet(userList);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
 
     private void showToast(String message){
         ToastMessage.show(this, message);
     }
-
 }
+
