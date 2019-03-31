03a54d99ed201d9ddb9c805f189e18744e75a456
MAKATA <limbaga@ualberta.ca>
2019-03-30 21:35:18 -0600

    calling finish instead of starting a new MainHomeViewActivity

--- app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/ViewBorrowedBookActivity.java
+++ app/src/main/java/com/cmput301/w19t06/theundesirablejackals/activities/ViewBorrowedBookActivity.java
@@ -68,7 +68,6 @@ public class ViewBorrowedBookActivity extends AppCompatActivity {
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),MainHomeViewActivity.class));
                 finish();
             }
         });
